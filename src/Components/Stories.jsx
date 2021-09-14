1abf1bfd825120b1424b69be28a40f94b2632670
Liam <shadowteckst@gmail.com>
2021-09-14 14:47:05 -0700

    idk but im just pushing and then starting my own

--- src/Components/Stories.jsx
+++ src/Components/Stories.jsx
@@ -13,13 +13,17 @@ const Stories = ({ url }) => {
         <>
             {products.map((product, index) => {
                 return (
-                    <div key={index} className="card">
+                    <div className="con">
+                        <div className="card-container">
+                         <div key={index} className="card">
                         <h2 className="card-title">{product.title ? product.title : "No Title"}</h2>
                         <p className="card-content">{product.points} points by {product.author ? product.author : "No Author"} | {product.num_comments ? product.num_comments : '0'} comments</p>
                         <div className="links">
                             {product.url && <a href={product.url}>Read More</a>}
                             <button onClick={removeArticle}>Remove</button>
                         </div>
+                    </div>
+                    </div>
                     </div>
                 )
             })}
